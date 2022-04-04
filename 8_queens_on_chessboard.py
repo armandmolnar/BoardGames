32d02f7762f5278c2032892e64c69644046cc94f
Armand Molnar <molnar.armand@gmail.com>
2022-04-04 14:34:29 +0200

    8queen init board

--- 8_queens_on_chessboard.py
+++ 8_queens_on_chessboard.py
@@ -9,16 +9,31 @@ def create_board(row=8, col=8):
 
 
 initial_board = create_board()
-initial_board[1, 1] = "queen"
+initial_board[2, 2] = "queen"
 positions_of_queens = [k for k, v in initial_board.items() if v == 'queen']
 for g in positions_of_queens:
-    for c in range(8):
+    for n in range(8):
         # rows of the queens: free->forbidden
-        if initial_board[g[0], c] == "free":
-            initial_board[g[0], c] = "forbidden"
+        if initial_board[g[0], n] == "free":
+            initial_board[g[0], n] = "forbidden"
         # columns of the queens: free->forbidden
-        if initial_board[c, g[1]] == "free":
-            initial_board[c, g[1]] = "forbidden"
+        if initial_board[n, g[1]] == "free":
+            initial_board[n, g[1]] = "forbidden"
+        # left_and_up axes of the queens: free->forbidden
+        if g[0] - n >= 0 and g[1] - n >= 0 and initial_board[g[0] - n, g[1] - n] == "free":
+            initial_board[g[0] - n, g[1] - n] = "forbidden"
+        # left_and_down axes of the queens: free->forbidden
+        if g[0] + n <= 7 and g[1] - n >= 0 and initial_board[g[0] + n, g[1] - n] == "free":
+            initial_board[g[0] + n, g[1] - n] = "forbidden"
+        # right_and_up axes of the queens: free->forbidden
+        if g[0] - n >= 0 and g[1] + n >=7 and initial_board[g[0] - n, g[1] + n] == "free":
+            initial_board[g[0] - n, g[1] + n] = "forbidden"
+        # right_and_down axes of the queens: free->forbidden
+        if g[0] + n <= 7 and g[1] + n <= 7 and initial_board[g[0] + n, g[1] + n] == "free":
+            initial_board[g[0] + n, g[1] + n] = "forbidden"
+
+
+
 
 positions_of_not_free = [k for k, v in initial_board.items() if v != 'free']
 print(positions_of_not_free)
